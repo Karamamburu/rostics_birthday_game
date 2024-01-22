42b7ed4f0dde9dbd1f77972bff5e9ab7e3fbae0a
KHMYROV Denis <denis.e.khmyrov@gmail.com>
2024-01-22 22:29:37 +0300

    fix: labirint changed, ghosts became more dangerous

--- src/const.js
+++ src/const.js
@@ -1,6 +1,6 @@
-const deadSound = './assets/sounds/packman_death.mp3'
-const moveSound = './assets/sounds/packman_waka.mp3'
-const eatSound = './assets/sounds/packman_eatfruit.mp3'
-const sountrackSound = './assets/sounds/packman_sountrack.mp3'
+const deadSound = './assets/sounds/pacman_death.mp3'
+const moveSound = './assets/sounds/pacman_waka.mp3'
+const eatSound = './assets/sounds/pacman_eatfruit.mp3'
+const sountrackSound = './assets/sounds/pacman_soundtrack.mp3'
 
 export { deadSound, moveSound, eatSound, sountrackSound }
